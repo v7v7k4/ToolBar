4d6ca372f7361cafe556de800c637ebe161b7b57
Vivian Souza <viviantsd@gmail.com>
2019-03-06 13:21:49 -0300

    ToolBar - android material design - curso udemy

---
+++ app/src/main/java/c/w/toolbar/MainActivity.java
@@ -0,0 +1,51 @@
+package c.w.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search_menu: {
+                Toast.makeText(this, "Buscar", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.carrinho_menu: {
+                Toast.makeText(this, "Carrinho", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.config_menu: {
+                Toast.makeText(this, "Configurações", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.sobre_menu: {
+                Toast.makeText(this, "Sobre", Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
